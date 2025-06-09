c05f4560789771988a7c70cdad647b854584f94b
Zuma <zuma@zuma.eu.org>
2025-06-10 00:38:22 +0100

    Added array state serialization

--- resolve/serialize.go
+++ resolve/serialize.go
@@ -16,7 +16,48 @@ func serialize(state *lua.State) any {
 	}
 }
 
-func serializeTable(state *lua.State) map[string]any {
+func isArray(state *lua.State) bool {
+	state.PushNil()
+	if !state.Next(-2) {
+		return true
+	}
+	isArray := state.IsNumber(-2)
+	state.Pop(2)
+	return isArray
+}
+
+func serializeTable(state *lua.State) any {
+	if isArray(state) {
+		return serializeArrayTable(state)
+	} else {
+		return serializeHashTable(state)
+	}
+}
+
+func serializeArrayTable(state *lua.State) []any {
+	length := getLength(state)
+	array := make([]any, length)
+	for i := range length {
+		state.RawGetInt(-1, i+1)
+		value := serialize(state)
+		array[i] = value
+		state.Pop(1)
+	}
+	return array
+}
+
+func getLength(state *lua.State) int {
+	state.Length(-1)
+	length, ok := state.ToInteger(-1)
+	if !ok {
+		err := errors.New("cannot get length of non-table")
+		utils.HandleErr(err)
+	}
+	state.Pop(1)
+	return length
+}
+
+func serializeHashTable(state *lua.State) map[string]any {
 	table := make(map[string]any)
 	state.PushNil()
 	for state.Next(-2) {
@@ -29,5 +70,6 @@ func serializeTable(state *lua.State) map[string]any {
 		}
 		table[key] = value
 	}
+	state.Pop(1)
 	return table
 }
