a1686d514fbff61b0d8799138e0f18fcb488c1f0
Zuma <zuma@zuma.eu.org>
2025-06-10 18:41:23 +0100

    Added manifest file

---
+++ manifest.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+)
+
+type ManifestFile struct {
+	Version string `json:"version"`
+}
+
+//go:embed manifest.json
+var manifest []byte
+var Manifest ManifestFile
+
+func init() {
+	json.Unmarshal(manifest, &Manifest)
+}
