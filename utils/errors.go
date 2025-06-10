0623ce4ee9fa3973a01f6aad517f337a95870f37
Zuma <zuma@zuma.eu.org>
2025-06-10 19:05:23 +0100

    Added comments to utils

--- utils/errors.go
+++ utils/errors.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// If an error is not nil, it writes it to stderr and exits with status code 1
 func HandleErr(err error) {
 	if err != nil {
 		fmt.Fprint(os.Stderr, "error: ")
--- utils/perms.go
+++ utils/perms.go
@@ -1,6 +1,9 @@
 package utils
 
+// File perms
 const (
+	// Executable for everyone, writable for owner, everyone else can read
 	EXECUTABLE_PERMS = 0755
-	READWRITE_PERMS  = 0644
+	// Writable for owner, readable for everyone, executable for no-one
+	READWRITE_PERMS = 0644
 )
