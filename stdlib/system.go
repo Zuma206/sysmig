dd7250579c81c5ea03d630cf8ede6e677bc87d58
Zuma <zuma@zuma.eu.org>
2025-06-13 04:09:06 +0100

    Broke system function out into subfunctions

--- stdlib/system.go
+++ stdlib/system.go
@@ -26,40 +26,13 @@ var systemMigratorFunc = LuaFunc{
 	Body: func(state *lua.State) int {
 		migratorsIndex := lua.UpValueIndex(1)
 		currentStateIndex := 1
-		currentStateNil := state.IsNil(currentStateIndex)
 		nMigrators := state.RawLength(migratorsIndex)
 		pushSystemResolution(state, nMigrators)
 		resolutionIndex := 2
 		ResolutionNextState.Push(state, resolutionIndex)
 		nextStateIndex := 3
-
-		for i := range nMigrators {
-			state.RawGetInt(migratorsIndex, i+1)
-			MigratorFunc.Push(state, -1)
-			if currentStateNil {
-				state.PushNil()
-			} else {
-				MigratorName.Push(state, -2)
-				state.Table(currentStateIndex)
-			}
-			state.Call(1, 1)
-			MigratorName.Push(state, -2)
-			ResolutionNextState.Push(state, -2)
-			state.SetTable(nextStateIndex)
-		}
-
-		for range nMigrators {
-			ResolutionMigration.Push(state, -1)
-			ResolutionMigration.Push(state, resolutionIndex)
-			state.Concat(2)
-			ResolutionMigration.Set(state, resolutionIndex)
-			ResolutionSync.Push(state, -1)
-			ResolutionSync.Push(state, resolutionIndex)
-			state.Concat(2)
-			ResolutionSync.Set(state, resolutionIndex)
-			state.Pop(2)
-		}
-
+		runMigrations(state, nMigrators, migratorsIndex, currentStateIndex, nextStateIndex)
+		collapseMigrations(state, nMigrators, resolutionIndex)
 		state.Pop(1)
 		return 1
 	},
@@ -73,4 +46,41 @@ func pushSystemResolution(state *lua.State, nMigrators int) {
 	ResolutionMigration.Set(state, -2)
 	state.PushString("")
 	ResolutionSync.Set(state, -2)
+}
+
+func runMigrations(
+	state *lua.State,
+	nMigrators int,
+	migratorsIndex int,
+	currentStateIndex int,
+	nextStateIndex int,
+) {
+	for i := range nMigrators {
+		state.RawGetInt(migratorsIndex, i+1)
+		MigratorFunc.Push(state, -1)
+		if state.IsNil(currentStateIndex) {
+			state.PushNil()
+		} else {
+			MigratorName.Push(state, -2)
+			state.Table(currentStateIndex)
+		}
+		state.Call(1, 1)
+		MigratorName.Push(state, -2)
+		ResolutionNextState.Push(state, -2)
+		state.SetTable(nextStateIndex)
+	}
+}
+
+func collapseMigrations(state *lua.State, nMigrators int, resolutionIndex int) {
+	for range nMigrators {
+		ResolutionMigration.Push(state, -1)
+		ResolutionMigration.Push(state, resolutionIndex)
+		state.Concat(2)
+		ResolutionMigration.Set(state, resolutionIndex)
+		ResolutionSync.Push(state, -1)
+		ResolutionSync.Push(state, resolutionIndex)
+		state.Concat(2)
+		ResolutionSync.Set(state, resolutionIndex)
+		state.Pop(2)
+	}
 }
