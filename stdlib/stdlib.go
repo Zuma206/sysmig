45b8d4ef1d46e97cbbe94912ded9f0550e516212
Zuma <zuma@zuma.eu.org>
2025-06-13 04:14:36 +0100

    Added documentation to stdlib.go

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -2,6 +2,7 @@ package stdlib
 
 import "github.com/Shopify/go-lua"
 
+// Opens all standard library functions/tables into the global scope
 func OpenLibraries(state *lua.State) {
 	state.PushGlobalTable()
 	System.Open(state)
