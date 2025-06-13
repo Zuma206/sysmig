f8dc4514f3e0a5af96751517477264e9dad55456
Zuma <zuma@zuma.eu.org>
2025-06-13 04:13:16 +0100

    Added documentation to migrators.go

--- stdlib/migrators.go
+++ stdlib/migrators.go
@@ -4,11 +4,13 @@ import (
 	"github.com/Shopify/go-lua"
 )
 
+// Represents attributes on a 'migrator' table
 var (
 	MigratorName = LuaAttribute[string]{"name", lstring, "migrator"}
 	MigratorFunc = LuaAttribute[lua.Function]{"func", lfunction, "migrator"}
 )
 
+// Represents attributes on a 'resolution' table
 var (
 	ResolutionMigration = LuaAttribute[string]{"migration", lstring, "resolution"}
 	ResolutionSync      = LuaAttribute[string]{"sync", lstring, "resolution"}
