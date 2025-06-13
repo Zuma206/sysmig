1590353de16e3146c038753b2066e3d04a74b38a
Zuma <zuma@zuma.eu.org>
2025-06-13 02:08:19 +0100

    Added nil and union types

--- stdlib/types.go
+++ stdlib/types.go
@@ -2,6 +2,23 @@ package stdlib
 
 import "github.com/Shopify/go-lua"
 
+func LuaUnion[A any, B any](typeA LuaType[A], typeB LuaType[B]) LuaType[any] {
+	return LuaType[any]{
+		Name: typeA.Name + "|" + typeB.Name,
+		Getter: func(state *lua.State, index int) (any, bool) {
+			a, aOk := typeA.Getter(state, index)
+			if aOk {
+				return a, aOk
+			}
+			b, bOk := typeB.Getter(state, index)
+			if bOk {
+				return b, bOk
+			}
+			return a, false
+		},
+	}
+}
+
 var (
 	ltable = LuaType[any]{"table", func(state *lua.State, index int) (any, bool) {
 		value := state.ToValue(index)
@@ -16,5 +33,8 @@ var (
 			return state.ToGoFunction(index), true
 		}
 		return nil, state.IsFunction(index)
+	}}
+	lnil = LuaType[any]{"nil", func(state *lua.State, index int) (any, bool) {
+		return nil, state.IsNil(index)
 	}}
 )
