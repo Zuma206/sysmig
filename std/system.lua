a5778b88127c9094ab789f9846b41440bc2f67e5
Zuma <zuma@zuma.eu.org>
2025-06-14 15:18:48 +0100

    Added initial_state feature to std.system

--- std/system.lua
+++ std/system.lua
@@ -1,11 +1,18 @@
 local migrator = require "@std.migrator"
 
+local function get_initial_state(migrators)
+  if migrators.initial_state then
+    return migrators.initial_state.func(nil).next_state
+  end
+  return {}
+end
+
 -- A migrator multiplexer
 -- Takes a list of migrators, splits the state, and combines the resolution
 return function(migrators)
   return migrator("std.system", function(current_state)
     local sys_resolution = { migration = "", sync = "", next_state = {} }
-    current_state = current_state or {}
+    current_state = current_state or get_initial_state(migrators)
     for _, i_migrator in ipairs(migrators) do
       local resolution = i_migrator.func(current_state[i_migrator.name])
       sys_resolution.migration = sys_resolution.migration .. "\n" .. resolution.migration
