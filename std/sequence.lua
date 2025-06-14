5c23b41af34019e0ac71fec93859cc2227e89af2
Zuma <zuma@zuma.eu.org>
2025-06-14 14:05:52 +0100

    Refactored std.sequence

--- std/sequence.lua
+++ std/sequence.lua
@@ -2,6 +2,30 @@ local migrator = require "@std.migrator"
 local set = require "@std.set"
 local map = require "@std.map"
 
+local function get_group_script(list, handle, handle_no)
+  if #list == 0 or handle == nil then
+    return handle_no .. "\n"
+  elseif handle then
+    return handle(list) .. "\n"
+  end
+  return ""
+end
+
+local function get_iterative_script(list, handle)
+  if handle then
+    return table.concat(map(list, handle), "\n") .. "\n"
+  end
+  return ""
+end
+
+local function get_sequence_script(added, removed, handlers)
+  handlers = handlers or {}
+  return get_group_script(added, handlers.added, handlers.no_added or "# Nothing to add") ..
+      get_group_script(removed, handlers.removed, handlers.no_removed or "# Nothing to remove") ..
+      get_iterative_script(added, handlers.add) ..
+      get_iterative_script(removed, handlers.remove)
+end
+
 -- Creates a migrator that acts upon a set, adding various commands for added/removed items
 return function(name, desired_sequence, handlers)
   return migrator(name, function(current_sequence)
@@ -12,18 +36,8 @@ return function(name, desired_sequence, handlers)
     local removed = set.to_sequence(set.diff(current, desired))
     return {
       next_state = desired_sequence,
-      migration = table.concat({
-        table.concat(map(added, function(item) return handlers.add(item) end), "\n"),
-        table.concat(map(removed, function(item) return handlers.remove(item) end), "\n"),
-        handlers.added(added),
-        handlers.removed(removed),
-      }, "\n"),
-      sync = table.concat({
-        table.concat(map(added, function(item) return handlers.add_sync(item) end), "\n"),
-        table.concat(map(removed, function(item) return handlers.remove_sync(item) end), "\n"),
-        handlers.added_sync(added),
-        handlers.removed_sync(removed),
-      }, "\n")
+      migration = get_sequence_script(added, removed, handlers.migration),
+      sync = get_sequence_script(added, removed, handlers.sync)
     }
   end)
 end
