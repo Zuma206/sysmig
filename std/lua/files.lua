74d42809c5bd8fd843c07413eb811be6233d1880
Zuma <zuma@zuma.eu.org>
2025-06-24 00:41:25 +0100

    Updated @std.files to working order

--- std/lua/files.lua
+++ std/lua/files.lua
@@ -1,13 +1,33 @@
 local serialize = require "@std.serialize"
 local sequence = require "@std.sequence"
 local entries = require "@std.entries"
+local dir = require "@std.dir"
+local path = require "@std.path"
+
+local function pass_paths(func)
+  return function(file)
+    local destination = path(file[1])
+    local source = path(file[2])
+    return func(source, destination)
+  end
+end
+
+local function remove_file(_, destination)
+  return "rm -rf " .. destination
+end
+
+local function copy_file(source, destination)
+  return table.concat({
+    "# Copying " .. source .. " to " .. destination,
+    remove_file(nil, destination),
+    "mkdir -p " .. dir(destination),
+    "cp -r " .. source .. " " .. destination,
+  }, "\n")
+end
 
 return function(files)
   return sequence("std.files", entries(files), {
-    migration = {
-      add = function(file)
-        return "# Adding file " .. file[2] .. " to " .. file[1]
-      end
-    }
+    migration = { add = pass_paths(copy_file), remove = pass_paths(remove_file) },
+    sync = { add = pass_paths(copy_file) }
   }, serialize)
 end
