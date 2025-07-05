7bfa4fe8c147720285197767b00ea3f64de06c73
Zuma <zuma@zuma.eu.org>
2025-07-05 17:04:06 +0100

    Added @std.homebrew.packages

---
+++ std/lua/homebrew.lua
@@ -0,0 +1,17 @@
+local sequence = require "@std.sequence"
+
+local homebrew = {}
+
+function homebrew.packages(packages)
+  return sequence("std.homebrew.packages", packages, {
+    added = function(added)
+      return "brew install " .. table.concat(added, " ")
+    end,
+    removed = function(removed)
+      return "brew uninstall " .. table.concat(removed, " ")
+    end,
+    sync = "brew update"
+  })
+end
+
+return homebrew
